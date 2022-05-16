a4e9a57847aba27ef20056672ebd8abca46336f0
salmamuhamed <104239779+salmamuhamed@users.noreply.github.com>
2022-05-16 12:05:14 +0200

    Update systicktimer.c

--- systicktimer.c
+++ systicktimer.c
@@ -2,12 +2,12 @@
 #define NVIC_ST_CTRL_R (*((volatile unsigned long *)0xE000E010))
 #define NVIC_ST_RELOAD_R (*((volatile unsigned long *)0xE000E014))
 
-void systick_Init(void){
+void Systick_Init(void){
   NVIC_ST_CTRL_R = 0;
   NVIC_ST_RELOAD_R = 0x00666666;
   NVIC_ST_CTRL_R = 00000005;
 }
-VOID Systick_WAIt(unsigned long delay){
+VOID Systick_Wait(unsigned long delay){
   NVIC_ST_RELOAD_R = delay - 1;
   NVIC_ST_CURRENT_R = 0;
   while ((NVIC_ST_CTRL_R&0x00010000) == 0);
