33f9c2b1b89c60382f29f7492130ac6a05598d19
abdelrahman khaled <105611939+boda5718@users.noreply.github.com>
2022-05-18 14:42:07 +0200

    Update countdown timer.c

--- countdown timer.c
+++ countdown timer.c
@@ -2,6 +2,7 @@ void LCD_String_countdown (int mm ,int m , int ss , int s)
 {
 	while(1)
 	{
+		GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R |= 0x0E; //turning on the array of leds
 		lcd_4bits_cmd(0x1);
 		LCD_String("      ");
 	        lcd_4bits_data( mm + 48);
@@ -34,7 +35,8 @@ void LCD_String_countdown (int mm ,int m , int ss , int s)
 		}
 		else if(s==0 & ss==0 & m==0 & mm==0)
 		{
-			buzzer();
+			Buzzer_Blink();
+			GPIO_PORTF_DATA_R =GPIO_PORTF_DATA_R &=~ 0x0E; //turning off the array of leds
 			return;
 		}
 		
