d2b9add8dd2f3d13a8d1b2359a78faee423b7762
salmamuhamed <104239779+salmamuhamed@users.noreply.github.com>
2022-05-23 19:43:52 +0200

    Update beef.c

--- beef.c
+++ beef.c
@@ -1,13 +1,15 @@
-void beef() 
+void beef()
 {
-  int total time , m ,ss , s;
-  unsigned char key;
-  lcd_4bits_cmd(0x1);
-  LCD_string("      beef");
-  delay_milli(1000);
- lcd_4bits_cmd(0x1);
-  LCD_String("enter number of");
-  lcd_4bits_cmd(0xc0);
+	int total_time , m , ss , s;
+	unsigned char key ;
+	int sw1, sw2;
+
+	lcd_4bits_cmd(0x1);
+	LCD_String("      beef");
+	delay_milli(1000);
+	loop_beef: lcd_4bits_cmd(0x1);
+	LCD_String("enter number of");
+	lcd_4bits_cmd(0xc0);
 	LCD_String("kilos(1~9) ");
 	lcd_4bits_cmd(0x0f);
 	
@@ -15,28 +17,49 @@ void beef()
 	{
 		key = get_key();
 		delay_milli(100);
-		if(48<key<58)
+			
+	} while(key==0 );
+	
+	if(48>key | key>58)
+		{
+			lcd_4bits_cmd(0x1);
+			LCD_String("wrong input");
+			lcd_4bits_cmd(0x0c);
+		  delay_milli(2000);
+			goto loop_beef;
+		}
+	else if(48<key & key<58)
 		{
 		lcd_4bits_data(key);
 		lcd_4bits_cmd(0x0c);
 		LCD_String("kilo");
-		delay_milli(2000);
-		} 
-		else
+		delay_milli(200);
+			
+			while(1)
+	{
+	
+				sw1 = sw_input() & 0x10;
+		    sw2 = sw_input() & 0x1;
+	
+		
+		
+		
+		 if (sw1 ==0x0)
 		{
-			lcd_4bits_cmd(0x1);
-			LCD_String("wrong input");
-		  delay_milli(3000);
-			beef();
+			goto loop_beef;
+		}
+		else if(sw2 ==0x0)
+		{
+			total_time = 30 * (key - 48);
+			m = total_time/60;
+			ss = (total_time - m*60)/10;
+			s=0;
+			LCD_String_countdown(0,m,ss,s);
 			return;
 		}
 			
-	} while(key==0);
-	total_time = 30 * (key - 48);
-	m = total_time/60;
-	ss = (total_time - m*60)/10;
-	s=0;
-	LCD_String_countdown(0,m,ss,s);
+		} 
+	
 	
 }
-  
+	}
